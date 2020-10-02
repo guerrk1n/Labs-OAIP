fcac770f14a69dbb43a6984ba42826051ca180a6
guerrk1n <aleksej684549@inbox.ru>
2020-10-02 23:44:54 +0300

    working version

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 
-const int array_length1 = 5, array_length2 = 7;
+const int ARRAY_LENGTH1 = 5, ARRAY_LENGTH2 = 7;
 int main()
 {
     setlocale(LC_ALL, "Russian");
     srand(time(NULL));
-    int array1[array_length1], array2[array_length2],buffer, min, counter=0, choice;
+    int array1[ARRAY_LENGTH1], array2[ARRAY_LENGTH2],buffer, min, counter=0, choice;
     
     cout << "Выберите вариант заполения массивов:" << endl;
     cout << "1. Автоматически" << endl;
@@ -19,12 +19,12 @@ int main()
     switch (choice) {
         
     case 1:
-        for (int i = 0; i < array_length1; i++) {
+        for (int i = 0; i < ARRAY_LENGTH1; i++) {
             
             array1[i] = rand() % 100 - rand() % 100;
             
         }
-        for (int i = 0; i < array_length2; i++) {
+        for (int i = 0; i < ARRAY_LENGTH2; i++) {
             array2[i] = rand() % 100 - rand() % 100;
         }
 
@@ -32,12 +32,12 @@ int main()
     
     case 2:
 
-        for (int i = 0; i < array_length1; i++) {
+        for (int i = 0; i < ARRAY_LENGTH1; i++) {
             cout << "Введите значение array1[" << i << "]: ";
             cin >> array1[i];
         }
 
-        for (int i = 0; i < array_length2; i++) {
+        for (int i = 0; i < ARRAY_LENGTH2; i++) {
             cout << "Введите значение array2[" << i << "]: ";
             cin >> array2[i];
         }
@@ -50,20 +50,20 @@ int main()
 
     if (choice == 1) {
         cout << "Полученные случайные массивы чисел:" << endl;
-        for (int i = 0; i < array_length1; i++) {
+        for (int i = 0; i < ARRAY_LENGTH1; i++) {
             cout << "array1[" << i << "]: " << array1[i] << "\n";
 
         }
 
         cout << "------------------------------" << endl;
-        for (int i = 0; i < array_length2; i++) {
+        for (int i = 0; i < ARRAY_LENGTH2; i++) {
             cout << "array2[" << i << "]: " << array2[i] << "\n";
 
         }
     }
     // Сортировка методом пузырька
-    for (int i = 0; i < array_length1; i++){
-        for (int j = 1; j < array_length1; j++) {
+    for (int i = 0; i < ARRAY_LENGTH1; i++){
+        for (int j = 1; j < ARRAY_LENGTH1; j++) {
             if (array1[j] < array1[j -1]) {
                 buffer = array1[j];
                 array1[j] = array1[j - 1];
@@ -73,8 +73,8 @@ int main()
         }
     }
     min = array1[0];
-    for (int i = 0; i < array_length1; i++) {
-        for (int j = 0; j < array_length2; j++) {
+    for (int i = 0; i < ARRAY_LENGTH1; i++) {
+        for (int j = 0; j < ARRAY_LENGTH2; j++) {
             if (array1[i] != array2[j]) {
                 counter++;
             }
@@ -83,12 +83,12 @@ int main()
                 break;
             }
         }
-        if (counter == array_length2) {
+        if (counter == ARRAY_LENGTH2) {
             min = array1[i];
             cout << min << endl;
             break;
         }
-        if (i == array_length1 - 1) {
+        if (i == ARRAY_LENGTH1 - 1) {
             cout << "Наименьше значения среди элементов первого массива, которые не входят во второй, нет";
         }
     }
