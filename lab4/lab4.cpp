77f0e1370e14e181438e507fd1a93169a623d56d
guerrk1n <aleksej684549@inbox.ru>
2020-10-08 17:13:01 +0300

    adding comments

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -2,14 +2,14 @@
 #include <ctime>
 using namespace std;
 
-
 const int ARRAY_LENGTH1 = 5, ARRAY_LENGTH2 = 7;
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    srand(time(NULL));
+    setlocale(LC_ALL, "Russian"); // Функция устанавливает определенный параметр для поддержки кирилицы в консоли
+    srand(time(NULL)); // Функция srand выполняет инициализацию генератора случайных чисел
     int array1[ARRAY_LENGTH1], array2[ARRAY_LENGTH2],buffer, min, counter=0, choice;
-    
+
+    // Заполнение массива случайно, либо заполнение пользователем
     cout << "Выберите вариант заполения массивов:" << endl;
     cout << "1. Автоматически" << endl;
     cout << "2. В ручную" << endl;
@@ -20,23 +20,18 @@ int main()
         
     case 1:
         for (int i = 0; i < ARRAY_LENGTH1; i++) {
-            
             array1[i] = rand() % 100 - rand() % 100;
-            
         }
         for (int i = 0; i < ARRAY_LENGTH2; i++) {
             array2[i] = rand() % 100 - rand() % 100;
         }
-
         break;
     
     case 2:
-
         for (int i = 0; i < ARRAY_LENGTH1; i++) {
             cout << "Введите значение array1[" << i << "]: ";
             cin >> array1[i];
         }
-
         for (int i = 0; i < ARRAY_LENGTH2; i++) {
             cout << "Введите значение array2[" << i << "]: ";
             cin >> array2[i];
@@ -48,30 +43,30 @@ int main()
         return 0;
     }
 
+    // Вывод полученных массивов
     if (choice == 1) {
         cout << "Полученные случайные массивы чисел:" << endl;
         for (int i = 0; i < ARRAY_LENGTH1; i++) {
             cout << "array1[" << i << "]: " << array1[i] << "\n";
-
         }
 
         cout << "------------------------------" << endl;
         for (int i = 0; i < ARRAY_LENGTH2; i++) {
             cout << "array2[" << i << "]: " << array2[i] << "\n";
-
         }
     }
-    // Сортировка методом пузырька
+    // Сортировка первого массива методом пузырька
     for (int i = 0; i < ARRAY_LENGTH1; i++){
         for (int j = 1; j < ARRAY_LENGTH1; j++) {
             if (array1[j] < array1[j -1]) {
                 buffer = array1[j];
                 array1[j] = array1[j - 1];
                 array1[j - 1] = buffer;
-
             }
         }
     }
+
+    // Поиск наименьшего значения первого массива, который не входит во второй
     min = array1[0];
     for (int i = 0; i < ARRAY_LENGTH1; i++) {
         for (int j = 0; j < ARRAY_LENGTH2; j++) {
@@ -92,7 +87,6 @@ int main()
             cout << "Наименьше значения среди элементов первого массива, которые не входят во второй, нет";
         }
     }
-
     return 0;
 }
 
